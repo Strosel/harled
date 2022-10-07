260748bfc20c5cd3a68870fb4f374940befec5f5
Axel Paulander <axel.paulander@gmail.com>
2022-10-07 18:19:07 +0200

    Use `#[harled()]` attribute instead of raw attributes (#2)

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -7,7 +7,7 @@ mod single_derive;
 
 pub(crate) use harled_core::Kind;
 
-#[proc_macro_derive(FromDeriveInput, attributes(Struct, Enum, Union))]
+#[proc_macro_derive(FromDeriveInput, attributes(harled))]
 #[allow(clippy::needless_return)] //used with rustfmt::skip to trick rustfmt
 pub fn derive_from_derive_input(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
@@ -23,7 +23,22 @@ pub fn derive_from_derive_input(input: TokenStream) -> TokenStream {
             let kind = ast
                 .attrs
                 .iter()
-                .map(|attr| attr.path.get_ident().unwrap().to_string().parse::<Kind>())
+                .filter_map(|attr| {
+                    (attr.path.get_ident().unwrap() == "harled").then(|| {
+                        //syn parse to proc_macro2::Group ?
+                        let group: proc_macro2::Group = syn::parse(attr.tokens.clone().into())
+                            .map_err(|_| "Attr is not a group")?;
+                        if group.delimiter() == proc_macro2::Delimiter::Parenthesis {
+                            Ok(group
+                                .stream()
+                                .to_string()
+                                .parse::<Kind>()
+                                .map_err(|_| "Attr is not a kind")?)
+                        } else {
+                            Err("Attr is not paren delimitered")
+                        }
+                    })
+                })
                 .fold(Count::None, |acc, res| match (&acc, res) {
                     (Count::None, Ok(k)) => Count::One(k),
                     (Count::One(_), Ok(_)) => Count::Many,
@@ -32,24 +47,30 @@ pub fn derive_from_derive_input(input: TokenStream) -> TokenStream {
 
             match kind {
                 Count::One(k) => single_derive::SingleDerive::new(k, ast).derive().into(),
-                _ => {
-                    #[rustfmt::skip]
-                    return syn::Error::new(
-                        ast.span(),
-                        format!(
-                            "FromDeriveInput on a struct requires exactly one of the following attributes `{}`, `{}`, `{}`",
-                             Kind::Struct, Kind::Enum, Kind::Union,
-                        )
-                    )
-                    .to_compile_error()
-                    .into();
-                }
+                Count::None => syn::Error::new(
+                    ast.span(),
+                    concat!(
+                        "FromDeriveInput requires the `#[harled(<Kind>)]` attribute,\n",
+                        "where `<Kind>` is a variant of harled_core::Kind"
+                    ),
+                )
+                .to_compile_error()
+                .into(),
+                Count::Many => syn::Error::new(
+                    ast.span(),
+                    concat!(
+                        "FromDeriveInput currently only supports one kind at a time for structs",
+                        "Try using an enum instead"
+                    ),
+                )
+                .to_compile_error()
+                .into(),
             }
         }
         syn::Data::Union(_) => {
             return syn::Error::new(
                 ast.span(),
-                "FromDeriveInput can only be derived for structs with attributes and enums",
+                "FromDeriveInput can only be derived for structs and enums",
             )
             .to_compile_error()
             .into();
